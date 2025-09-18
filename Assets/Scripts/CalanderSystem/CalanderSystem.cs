9f1d9a1c73d25eb0f0a64553c37e643187174d66
manav907 <manav907@gmail.com>
2025-09-18 12:48:07 +0530

    Move calendar record classes to separate file
    Extracted CalanderRecord, DateRecord, EventRecord, and TypeOfEvent classes from CalanderSystem.cs into a new CalanderRecord.cs file for better organization and maintainability.

--- Assets/Scripts/CalanderSystem/CalanderSystem.cs
+++ Assets/Scripts/CalanderSystem/CalanderSystem.cs
@@ -59,31 +59,3 @@ public class CalanderSystem : MonoBehaviour
         }
     }
 }
-// Allows creation from Unity context menu
-[CreateAssetMenu(fileName = "CalanderRecord", menuName = "Calendar/CalanderRecord")]
-public class CalanderRecord : ScriptableObject
-{
-    [SerializeField] public List<DateRecord> dates;//Only 30
-}
-[Serializable]
-public class DateRecord
-{
-    public string date;//used for Dictionary key and Reffe
-    public List<EventRecord> events;
-}
-[Serializable]
-public class EventRecord
-{
-    public string eventName;
-    public string eventDescription;
-    public TypeOfEvent eventType;
-}
-[Serializable]
-public enum TypeOfEvent
-{
-    ForcedCutscene,
-    FreeTime,
-    Speical,
-    SkipDayOrEvening,
-    GamePlay
-}
