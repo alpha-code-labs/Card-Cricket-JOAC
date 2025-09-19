e73b47f676fae8735c814f174e2d09444f1d4aff
manav907 <manav907@gmail.com>
2025-09-19 17:34:20 +0530

    Add dynamic location confirmation to dialogue
    Renamed TalkToLocationIntractionHandler to TalkToCharacterIntractionHandler. Added Yarn function 'get_next_location' to WorldIntractionDialougeManager for dynamic location display in dialogue. Updated ConfirmLocation.yarn to use the new function and improved dialogue options.

--- Assets/WorldIntractionDialougeManager.cs
+++ Assets/WorldIntractionDialougeManager.cs
@@ -12,9 +12,18 @@ public class WorldIntractionDialougeManager : MonoBehaviour
     {
         instance = this;
     }
+    void Start()
+    {
+        // dialogueRunner.AddFunction<string>("get_next_location", GetNextLocation);
+    }
     internal Locations location;
     public bool IsDialogueCurrentlyRunning()
     {
         return dialogueRunner.IsDialogueRunning;
+    }
+    [YarnFunction("get_next_location")]
+    public static string GetNextLocation()
+    {
+        return instance.location.ToString();
     }
 }
