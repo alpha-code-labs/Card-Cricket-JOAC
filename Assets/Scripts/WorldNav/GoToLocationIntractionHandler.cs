01d885d7ce596adc5f2d500704fd1ade79aa2c80
manav907 <manav907@gmail.com>
2025-09-19 16:29:48 +0530

    Add location confirmation dialogue to World Nav
    Introduces a Yarn dialogue for confirming location changes, adds WorldIntractionDialougeManager to manage dialogue interactions, and updates GoToLocationIntractionHandler to use the confirmation flow before switching locations. Removes unused intractionType fields from scene objects.

--- Assets/Scripts/WorldNav/GoToLocationIntractionHandler.cs
+++ Assets/Scripts/WorldNav/GoToLocationIntractionHandler.cs
@@ -1,14 +1,32 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Yarn.Unity;
 
 public class GoToLocationIntractionHandler : ClickAbleObjectHandler
 {
+    public static bool selectingLocation = false;
     public Locations location;
     public override void OnClick()
     {
-        Debug.Log("Clicked on " + gameObject.name);
-        LocationSwitcher.instance.SwitchLocation(location);
+        // LocationSwitcher.instance.SwitchLocation(location);
+        if (selectingLocation) return;
+        selectingLocation = true;
+        WorldIntractionDialougeManager.instance.location = location;
+        WorldIntractionDialougeManager.instance.dialogueRunner.StartDialogue("ConfirmLocation");
+    }
+    [YarnCommand("confirm_location")]
+    public static void ConfirmLocation(bool userConfirmed)
+    {
+        if (userConfirmed)
+        {
+            LocationSwitcher.instance.SwitchLocation(WorldIntractionDialougeManager.instance.location);
+        }
+        else
+        {
+            Debug.Log("User cancelled location switch.");
+        }
+        selectingLocation = false;
     }
 }
 public enum Locations
---
+++ Assets/WorldIntractionDialougeManager.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Yarn;
+using Yarn.Unity;
+
+public class WorldIntractionDialougeManager : MonoBehaviour
+{
+    public static WorldIntractionDialougeManager instance;
+    [SerializeField] internal DialogueRunner dialogueRunner;
+    void Awake()
+    {
+        instance = this;
+    }
+    internal Locations location;
+}
