95096c2d8c4019829263205aaf36c41e7aca2101
manav907 <manav907@gmail.com>
2025-09-22 17:53:16 +0530

    Integrate CalanderSystem and event handling for new day
    Added CalanderSystem as a singleton and linked it to NewDayManager for event management. NewDayManager now initializes and starts events based on the current date, with event handling logic moved from CalanderSystem. Updated GameManager to reset event index on start and modified MainMenu scene to include CalanderSystem. Updated build settings to include new scenes.

--- Assets/NewDayManager.cs
+++ Assets/NewDayManager.cs
@@ -5,10 +5,48 @@ using UnityEngine;
 
 public class NewDayManager : MonoBehaviour
 {
+    public static NewDayManager instance;
     TextMeshProUGUI dateText;
+    public static int currentEventIndex = 0;//Dont Modify This Directly you are probably making a mistake if you want to
+    DateRecord currentDateRecord;
     void Start()
     {
         dateText = GetComponentInChildren<TextMeshProUGUI>();
         dateText.text = GameManager.instance.currentSaveData.currentDate;
+        currentDateRecord = CalanderSystem.instance.GetDateRecordFromDate(GameManager.instance.currentSaveData.currentDate);
+        StartEvent(currentDateRecord.events[currentEventIndex]);
     }
+    public string GetCurrentEventName()
+    {
+        return currentDateRecord.events[currentEventIndex].eventName;
+    }
+    public void StartEvent(EventRecord events)
+    {
+        Debug.Log($"Starting Event: {events.eventName} of type {events.eventType}");
+        switch (events.eventType)
+        {
+            case TypeOfEvent.ForcedCutscene:
+                // LoadDialoageSystem(events.eventName);  // dialogueRunner.StartDialogue("FirstTimeIntro");
+
+                //Load cutscene
+                break;
+            case TypeOfEvent.FreeTime:
+                //Load FreeTime
+                break;
+            case TypeOfEvent.Speical:
+                //Load Special Event
+                break;
+            case TypeOfEvent.SkipDayOrEvening:
+                //Skip to next day or evening
+                break;
+            case TypeOfEvent.GamePlay:
+                ScoreManager.Instance.SetTargetFromEventName(events.eventName);
+                //Load GamePlay                
+                break;
+            default:
+                UnityEngine.Debug.LogError("No event type found");
+                break;
+        }
+    }
+
 }
