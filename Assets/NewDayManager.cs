d6f6083ee4b784fa02a4ff6c0509d65eb2b4d2f5
manav907 <manav907@gmail.com>
2025-09-23 10:03:34 +0530

    Add scene transitions for event types in NewDayManager
    Scene transitions are now triggered based on event type in NewDayManager.cs, using TransitionScreenManager to load the appropriate scene for ForcedCutscene, FreeTime, and GamePlay events. Also added isEvening flag and minor code cleanup.

--- Assets/NewDayManager.cs
+++ Assets/NewDayManager.cs
@@ -2,13 +2,15 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class NewDayManager : MonoBehaviour
 {
     public static NewDayManager instance;
     TextMeshProUGUI dateText;
-    public static int currentEventIndex = 0;//Dont Modify This Directly you are probably making a mistake if you want to
     DateRecord currentDateRecord;
+    public static int currentEventIndex = 0;//Dont Modify This Directly you are probably making a mistake if you want to
+    bool isEvening = false;
     void Start()
     {
         dateText = GetComponentInChildren<TextMeshProUGUI>();
@@ -27,11 +29,10 @@ public class NewDayManager : MonoBehaviour
         {
             case TypeOfEvent.ForcedCutscene:
                 // LoadDialoageSystem(events.eventName);  // dialogueRunner.StartDialogue("FirstTimeIntro");
-
-                //Load cutscene
+                TransitionScreenManager.instance.LoadScene("yarn-test");
                 break;
             case TypeOfEvent.FreeTime:
-                //Load FreeTime
+                TransitionScreenManager.instance.LoadScene("World-Nav");
                 break;
             case TypeOfEvent.Speical:
                 //Load Special Event
@@ -41,10 +42,11 @@ public class NewDayManager : MonoBehaviour
                 break;
             case TypeOfEvent.GamePlay:
                 ScoreManager.Instance.SetTargetFromEventName(events.eventName);
+                TransitionScreenManager.instance.LoadScene("CardGameScene");
                 //Load GamePlay                
                 break;
             default:
-                UnityEngine.Debug.LogError("No event type found");
+                Debug.LogError("No event type found");
                 break;
         }
     }
