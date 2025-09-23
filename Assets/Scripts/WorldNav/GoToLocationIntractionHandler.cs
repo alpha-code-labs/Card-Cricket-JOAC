8778f8acd662953c95bfef3060d16e6ef583abb4
manav907 <manav907@gmail.com>
2025-09-23 11:55:15 +0530

    Improve location name formatting in confirmation dialog
    Added AddSpacesToEnum method to format enum names with spaces for better readability in confirmation dialog choices. Updated dialogue to use formatted location names.

--- Assets/Scripts/WorldNav/GoToLocationIntractionHandler.cs
+++ Assets/Scripts/WorldNav/GoToLocationIntractionHandler.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Text.RegularExpressions;
 using UnityEngine;
 using Yarn.Unity;
 
@@ -8,11 +9,20 @@ public class GoToLocationIntractionHandler : ClickAbleObjectHandler
     [SerializeField] Locations location;
     public override void OnClick()
     {
-        WorldIntractionDialougeManager.instance.StartConfirmationDialogue("Yes, go to " + location.ToString(), "No, stay here", OnConfirmed);
+        WorldIntractionDialougeManager.instance.StartConfirmationDialogue(GetYesChoice(), "No, stay here", OnConfirmed);
+    }
+    string GetYesChoice()
+    {
+        string locationName = AddSpacesToEnum(location.ToString());
+        return "Yes, go to " + locationName;
     }
     void OnConfirmed()
     {
         LocationSwitcher.instance.SwitchLocation(location);
+    }
+    public static string AddSpacesToEnum(string enumName)
+    {
+        return Regex.Replace(enumName, "([a-z])([A-Z])", "$1 $2");
     }
 
 }
