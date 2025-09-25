0a377b631a5a4585cc80e9bb9c7b5ea0f1309d75
manav907 <manav907@gmail.com>
2025-09-25 16:15:04 +0530

    Add currency type support to CurrencyUI
    Introduces a serialized currencyType field to CurrencyUI and updates the UI to display the correct currency value based on the selected type. Updates prefabs and scene references to set currencyType for each currency display.

--- Assets/Scripts/UI/Currency/CurrencyUI.cs
+++ Assets/Scripts/UI/Currency/CurrencyUI.cs
@@ -6,6 +6,29 @@ using UnityEngine.EventSystems;
 
 public class CurrencyUI : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
 {
+    [SerializeField] TextMeshProUGUI currencyText;//Amount
+    [SerializeField] Reward currencyType;
+    void Start()
+    {
+        switch (currencyType)
+        {
+            case Reward.Courage:
+                currencyText.text = GameManager.instance.currentSaveData.courage.ToString();
+                break;
+            case Reward.Humility:
+                currencyText.text = GameManager.instance.currentSaveData.humility.ToString();
+                break;
+            case Reward.Foresight:
+                currencyText.text = GameManager.instance.currentSaveData.foresight.ToString();
+                break;
+            case Reward.Resourcefulness:
+                currencyText.text = GameManager.instance.currentSaveData.resourcefulness.ToString();
+                break;
+            default:
+                currencyText.text = "0";
+                break;
+        }
+    }
     [SerializeField] string tooltipText;
     public void OnPointerEnter(PointerEventData eventData)
     {
