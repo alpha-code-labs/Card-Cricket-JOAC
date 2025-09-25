25508f74ec4099f1cdf7ae63e9b80a6c6beaf9d5
manav907 <manav907@gmail.com>
2025-09-25 10:54:32 +0530

    Add reward image display to dialogue system
    Introduces a reward image feature to the YarnDialogSystemSingleTonMaker, including a mapping from Reward enum values to sprites and a method to show and fade the reward image. Updates NewDayScene.unity to include the necessary UI Image and sprite references.

--- Assets/Scripts/Global/YarnDialogSystemSingleTonMaker.cs
+++ Assets/Scripts/Global/YarnDialogSystemSingleTonMaker.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
+using DG.Tweening;
 using UnityEngine;
+using UnityEngine.UI;
 using Yarn.Unity;
 
 public class YarnDialogSystemSingleTonMaker : MonoBehaviour
@@ -21,7 +23,7 @@ public class YarnDialogSystemSingleTonMaker : MonoBehaviour
     void Start()
     {
         dialogueRunner = GetComponent<DialogueRunner>();
-
+        ProcessSprites();
     }
     public DialogueRunner dialogueRunner;
     [YarnCommand("AutoAdvance")]
@@ -29,4 +31,57 @@ public class YarnDialogSystemSingleTonMaker : MonoBehaviour
     {
         instance.dialogueRunner.GetComponentInChildren<LinePresenter>().autoAdvance = isAuto;
     }
+    [SerializeField] Image RewardImage;
+    [SerializeField] List<Sprite> sprites;
+    Dictionary<Reward, Sprite> rewardSpriteDict;
+    void ProcessSprites()
+    {
+        RewardImage.gameObject.SetActive(false);
+        rewardSpriteDict = new Dictionary<Reward, Sprite>();
+        foreach (var sprite in sprites)
+        {
+            Reward reward;
+            string spriteName = sprite.name.Split('-')[0]; // Remove '-' and everything after
+            if (System.Enum.TryParse<Reward>(spriteName, out reward))
+            {
+                rewardSpriteDict[reward] = sprite;
+            }
+            else
+            {
+                Debug.LogWarning($"Sprite with name {sprite.name} does not match any Reward enum value.");
+            }
+        }
+    }
+    [YarnCommand("ShowRewardImage")]
+    [ContextMenu("ShowRewardImage")]
+    public static void ShowRewardImage(string rewardType)
+    {
+        Reward reward;
+        System.Enum.TryParse<Reward>(rewardType, out reward);
+        Sprite sprite = instance.rewardSpriteDict[reward];
+        if (sprite != null)
+        {
+            instance.RewardImage.sprite = sprite;
+            instance.RewardImage.color = new Color(1, 1, 1, 0);
+            instance.RewardImage.gameObject.SetActive(true);
+            instance.RewardImage.DOFade(1, 0.5f).OnComplete(() =>
+            {
+                instance.RewardImage.DOFade(0, 0.5f).SetDelay(2f).OnComplete(() =>
+                {
+                    instance.RewardImage.gameObject.SetActive(false);
+                });
+            });
+        }
+        else
+        {
+            Debug.LogWarning($"Sprite with name {rewardType} not found in Resources/Sprites/Rewards/");
+        }
+    }
+}
+enum Reward
+{
+    Courage,
+    Foresight,
+    Humility,
+    Resourcefulness,
 }
