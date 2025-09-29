178246677eae75d30f569f3f629c616f1be7fba5
manav907 <manav907@gmail.com>
2025-09-29 14:12:41 +0530

    Comment out default currentDate assignment
    The assignment of 'currentDate' to '1990/03/15' for Cricket Gameplay 4 has been commented out, likely to prevent this value from being set by default during development or testing.

--- Assets/Scripts/Global/SaveData.cs
+++ Assets/Scripts/Global/SaveData.cs
@@ -20,7 +20,7 @@ public class SaveData
         // currentDate = "1990/03/02";//First FreeTime
         // currentDate = "1990/04/15";//Last Quiz Scene
         // currentDate = "1990/04/30";// Last day of Chapter 1
-        currentDate = "1990/03/15";// Cricket Gameplay 4
+        // currentDate = "1990/03/15";// Cricket Gameplay 4
 
 #endif
         humility = 0;
