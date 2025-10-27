e8b6266a60e29ac38af1c6e4738633c2988c104e
manav907 <manav907@gmail.com>
2025-10-27 16:45:14 +0530

    Swapped to Default ID for Now

--- Assets/Scripts/Platforms/SteamManager.cs
+++ Assets/Scripts/Platforms/SteamManager.cs
@@ -20,8 +20,7 @@ using Steamworks;
 // It handles the basics of starting up and shutting down the SteamAPI for use.
 //
 [DisallowMultipleComponent]
-public class SteamManager : MonoBehaviour
-{
+public class SteamManager : MonoBehaviour {
 #if !DISABLESTEAMWORKS
 	protected static bool s_EverInitialized = false;
 
@@ -170,12 +169,10 @@ public class SteamManager : MonoBehaviour
 		SteamAPI.RunCallbacks();
 	}
 #else
-    public static bool Initialized
-    {
-        get
-        {
-            return false;
-        }
-    }
+	public static bool Initialized {
+		get {
+			return false;
+		}
+	}
 #endif // !DISABLESTEAMWORKS
 }
--- Assets/Scripts/Platforms/SteamScript.cs
+++ Assets/Scripts/Platforms/SteamScript.cs
@@ -4,12 +4,33 @@ using Steamworks;
 
 public class SteamScript : MonoBehaviour
 {
-    void Start()
-    {
-        if (SteamManager.Initialized)
-        {
-            string name = SteamFriends.GetPersonaName();
-            Debug.Log(name);
-        }
-    }
+	void Start()
+	{
+		if (SteamManager.Initialized)
+		{
+			string name = SteamFriends.GetPersonaName();
+			Debug.Log(name);
+		}
+	}
+	protected Callback<GameOverlayActivated_t> m_GameOverlayActivated;
+
+	private void OnEnable()
+	{
+		if (SteamManager.Initialized)
+		{
+			m_GameOverlayActivated = Callback<GameOverlayActivated_t>.Create(OnGameOverlayActivated);
+		}
+	}
+
+	private void OnGameOverlayActivated(GameOverlayActivated_t pCallback)
+	{
+		if (pCallback.m_bActive != 0)
+		{
+			Debug.Log("Steam Overlay has been activated");
+		}
+		else
+		{
+			Debug.Log("Steam Overlay has been closed");
+		}
+	}
 }
