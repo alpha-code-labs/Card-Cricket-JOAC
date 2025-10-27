93d4c703e822884a14fc88b333aba61c44642173
manav907 <manav907@gmail.com>
2025-10-27 11:39:11 +0530

    Adding Node Skip

--- Assets/Scripts/DialogeManager/DialogueScriptCommandHandler.cs
+++ Assets/Scripts/DialogeManager/DialogueScriptCommandHandler.cs
@@ -57,7 +57,20 @@ public class DialogueScriptCommandHandler : MonoBehaviour
         {
             if (YarnDialogSystemSingleTonMaker.instance.dialogueRunner != null)
             {
+                Debug.Log($"Resrarting Current Node at: {currentNode}");
                 YarnDialogSystemSingleTonMaker.instance.dialogueRunner.StartDialogue(currentNode);
+            }
+            else
+            {
+                Debug.LogError("DialogueRunner not assigned!");
+            }
+        }
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            if (YarnDialogSystemSingleTonMaker.instance.dialogueRunner != null)
+            {
+                Debug.Log($"Ending Current Node at: {currentNode}");
+                YarnDialogSystemSingleTonMaker.instance.dialogueRunner.StartDialogue("EndEvent");
             }
             else
             {
