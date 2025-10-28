485d9ed36667bf1ea6fed5c7d0f1cb3c54d3ca1e
Ajay Singh <ajayaxes318@gmail.com>
2025-10-28 15:18:45 +0530

    updated gameplay 8 target score

--- Assets/Scripts/InGame/GameplayConfiguration.cs
+++ Assets/Scripts/InGame/GameplayConfiguration.cs
@@ -59,7 +59,7 @@ public class GameplayConfiguration : MonoBehaviour
             {"1990/03/16", new GameplayConfig(5, "1990/03/16", 60, 0, 0, 110, PitchCondition.Friendly)}, // Batting first
             {"1990/03/17", new GameplayConfig(6, "1990/03/17", 60, 125, 0, 125, PitchCondition.Friendly)}, // Chase 120 runs
             {"1990/04/11", new GameplayConfig(7, "1990/04/11", 90, 0, 0, 150,  PitchCondition.Friendly)}, // Batting first
-            {"1990/04/12", new GameplayConfig(8, "1990/04/12", 90, 0, 0, 90, PitchCondition.Friendly)}, // Batting first
+            {"1990/04/12", new GameplayConfig(8, "1990/04/12", 90, 0, 0, 145, PitchCondition.Friendly)}, // Batting first
             {"1990/04/13", new GameplayConfig(9, "1990/04/13", 90, 185, 0, 185, PitchCondition.Friendly)}, // Chase 185 runs
             
             // New gameplays (12-17)
